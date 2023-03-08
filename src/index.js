9e6f53f0675ca04a7ad0b73e989e530a01e7f74d
Protonm0KEN <gamerovichakbar@gmail.com>
2023-03-08 23:35:26 +0500

    adapted

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
+import "./styles/media-responsive-adapt/media.scss"
 import { Provider } from "react-redux";
 import store, { persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
